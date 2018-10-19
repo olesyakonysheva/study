bce8250ea72a977c045926c0886de99de9815793
Olesya Chumachenko <o_lesya88@mail.ru>
2018-10-19 14:32:26 +0300

    lesson 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import   CSSTransitionGroup   from 'react-transition-group/CSSTransitionGroup';
 import './App.css';
 
+import Form from './components/Form';
+
+
 class App extends Component {
+    state = {
+        mounted: true
+    };
+ handleSubmit = event => {
+     event.preventDefult();
+     this.setState({ mounted: false })
+    };
+    
     render() {
         return (
             <div className="app">
+            <CSSTransitionGroup
+                transitionName="fade"
+                transitionAppear={true}
+                transitionAppearTimeout={500}
+                transitionEnter={false}
+                transitionLeave={true}
+                transitionLeaveTimeout={300}>
+                {this.state.mounted && <Form onSubmit={this.handleSumbit} />}
+            </CSSTransitionGroup>
             </div>
         );
     }
